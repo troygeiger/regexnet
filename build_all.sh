f3ea7e922f56763cb4fd9872a882a724a86ff8dd
Troy Geiger <teiger80@gmail.com>
2021-02-07 00:33:14 -0500

    Changed build script to move all files to bin Clean-up after build

--- build_all.sh
+++ build_all.sh
@@ -10,8 +10,10 @@ AppName="regexnet"
 BuildVersion=$(echo $1 | cut -d 'v' -f 2)
 Bin=$(dirname "$(readlink -f $0)")/bin
 OutPathBase=$Bin/LinuxPackages
-OutPath="$OutPathBase/$AppName-$BuildVersion-amd64"
-OutPathArm64="$OutPathBase/$AppName-$BuildVersion-arm64"
+PkgName="$AppName-$BuildVersion-amd64"
+PkgNameArm="$AppName-$BuildVersion-arm64"
+OutPath="$OutPathBase/$PkgName"
+OutPathArm64="$OutPathBase/$PkgNameArm"
 
 if [ ! -d $OutPath/DEBIAN ]
 then
@@ -21,6 +23,9 @@ if [ ! -d "$OutPathArm64/DEBIAN" ]
 then
     mkdir -p "$OutPathArm64/DEBIAN"
 fi
+
+rm -f "$Bin/*.zip"
+rm -f "$Bin/*.deb"
 
 cat > $OutPath/DEBIAN/control << EOF
 Package: ${AppName}
@@ -46,6 +51,14 @@ dotnet publish -c Release -r linux-arm64 -o $OutPathArm64/usr/bin -p:Version=$Bu
 dotnet publish -c Release -r win-x64 -o $Bin/publish -p:IncludeNativeLibrariesForSelfExtract=true
 dpkg-deb --build $OutPath
 dpkg-deb --build $OutPathArm64
+mv "$OutPathBase/$PkgName.deb" "$Bin/"
+mv "$OutPathBase/$PkgNameArm.deb" "$Bin/"
+
 zip -j "$Bin/linux-x64.zip" "$OutPath/usr/bin/$AppName"
 zip -j "$Bin/linux-arm64.zip" "$OutPathArm64/usr/bin/$AppName"
 zip -j "$Bin/windows-x64.zip" "$Bin/publish/$AppName.exe"
+
+# Clean up
+rm -rf "$OutPathBase"
+rm -rf "$Bin/Release"
+rm -rf "$Bin/publish"
